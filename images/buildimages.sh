85d991922bfabcc39445eac15600be0d0d07b651
Pere Piñol <ppp3@alumnes.udl.cat>
2020-02-08 12:12:41 +0100

    Corrected repository address

--- images/buildimages.sh
+++ images/buildimages.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+if [ "$#" -eq 1 ]; then
+	registry=$1
+	echo "Set registry to $registry"
+fi
+
 echo "***** Searching for images to build *****"
 for dir in $(ls -F | grep '\/$'); do
   if [ -f "$dir/Dockerfile" ]; then
     echo "***** Found Dockerfile for $dir, building *****"
-    docker build $dir -t $(echo $dir | awk 'BEGIN{FS="/"}{print $1}')
+    imagename=$(echo $dir | awk 'BEGIN{FS="/"}{print $1}')
+    docker build $dir -t $imagename
     echo ""
+    
+    if [ ! -z "$registry" ]; then
+    	echo "***** Pushing image to registry ($registry/$imagename) *****"
+    	docker tag $imagename "$registry/$imagename"
+    	docker push "$registry/$imagename"
+    fi
   fi
 done
-echo "***** Images built, exiting *****"
+echo "***** Images built, cleaning *****"
+if [ ! -z "$registry" ]; then
+	docker image prune -f
+fi
+echo "***** Done, exiting *****"
