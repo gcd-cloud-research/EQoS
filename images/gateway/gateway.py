504e6d36d5f6d8861fd4b942968f75e7f21a4f78
Pere Piñol <perepinol@gmail.com>
2020-03-03 13:29:53 +0100

    Basic gateway logic completed

--- images/gateway/gateway.py
+++ images/gateway/gateway.py
@@ -3,41 +3,53 @@ import falcon
 import json
 from http.client import HTTPConnection
 
-DATA_DIR = "/routing"
+DATA_DIR = "/routing/"
 HEADERS = {'Content-Type': 'application/json'}
 
 
-def get_best_host(service_name):
-    candidates = filter(lambda candidate: service_name in candidate, os.listdir(DATA_DIR))
-    print(list(candidates))
-    if len(list(candidates)) == 0:
-        return None
+def parse_host(host_line):
+    parts = host_line.split()
+    host = parts[0]
+    port = parts[1].split("/")[0]
+    return "%s:%s" % (host, port)
 
-    best = list(candidates)[0]
-    with open(best) as fh:
-        best_host = fh.readline()
+
+def get_best_host(service_name):
+    candidates = list(filter(lambda candidate: service_name in candidate, os.listdir(DATA_DIR)))
+    if len(candidates) == 0:
+        return None
+    best = candidates[0]
+
+    with open(DATA_DIR + best) as fh:
+        best_host = parse_host(fh.read())
     return best_host
 
 
-def on_mongoapi(req, resp):
+def on_request(req, resp):
     try:
         body = json.load(req.bounded_stream)
     except json.JSONDecodeError:
         body = {}
 
-    host = get_best_host('mongoapi')
+    service = req.relative_uri.split("/")[1]
+    uri = "/".join(req.relative_uri.split("/")[2:])
+
+    host = get_best_host(service)
     if host is None:
-        return falcon.HTTP_404
+        resp.status = falcon.HTTP_404
+        return
 
     conn = HTTPConnection(host)
-    conn.request(req.method, req.relative_uri, body, HEADERS)
+    conn.request(req.method, uri, body, HEADERS)
+
     response = conn.getresponse()
+    print(response.getcode())
     if response.getcode() != 200:
         resp.status = falcon.HTTP_500
         return
     body = response.read().decode('utf-8')
-    resp.body = body
+    resp.body = json.dumps(body)
 
 
 api = falcon.API()
-api.add_sink(on_mongoapi, '/mongoapi/')
+api.add_sink(on_request, '/')
