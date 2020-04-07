cae8dd687fb185872f027e4fd1f89c2f0e524095
Pere Piñol <perepinol@gmail.com>
2020-04-07 08:27:29 -0700

    Moved job creation logic to Producer

--- images/producer/producer.py
+++ images/producer/producer.py
@@ -1,14 +1,71 @@
+import os
+import json
+
 from flask import Flask, request, abort
-from flask_cors import CORS
 from werkzeug.utils import secure_filename
+import requests
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+
+with open('/var/run/secrets/kubernetes.io/serviceaccount/token') as fh:
+    token = fh.read()
 
 app = Flask(__name__)
-CORS(app)
+config.load_incluster_config()
+kube_api = client.BatchV1Api()
 
 
 @app.route('/test')
 def test():
     return ''
+
+
+def create_job(routine_id):
+    """Fill job as per routine.yaml content."""
+    # Create job base object
+    job = client.V1Job(api_version='batch/v1', kind='Job')
+    # Fill with metadata
+    job.metadata = client.V1ObjectMeta(name=routine_id)
+    # Prepare template object
+    job.spec = client.V1JobSpec(
+        template=client.V1PodTemplateSpec(
+            spec=client.V1PodSpec(
+                containers=[client.V1Container(
+                    name=routine_id,
+                    image=routine_id,
+                    command=["wrapper.py", routine_id]
+                )],
+                restart_policy='Never'
+            )
+        ),
+        backoff_limit=4
+    )
+    return job
+
+
+def create_routine(routine_name):
+    # Initialise routine in database
+    res = requests.post('http://mongoapi:8000/routine/new', data=json.dumps({
+        'name': routine_name,
+        'issuer': '?'
+    }))
+    if res.status_code != 200:
+        app.logger.error("Error submitting routine to database: " + res.text)
+        abort(500)
+    routine_id = res.json()['id']  # Will represent the job in the database and the image
+    # Build Docker image for routine and upload to registry
+    if os.fork() == 0:
+        os.execl('./buildRoutine.sh', './buildRoutine.sh', routine_id)
+    os.wait()
+
+    # Submit job
+    job = create_job(routine_id)
+    try:
+        res = kube_api.create_namespaced_job('default', job, pretty=True)
+        app.logger.debug(res)
+    except ApiException as exception:
+        app.logger.error("Could not create job: %s" % exception)
+        abort(500)
 
 
 @app.route('/', methods=['POST'])
@@ -23,8 +80,9 @@ def new_routine():
         abort(400)
 
     f.save('/received/' + name)
+    create_routine(name)
     return ''
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
