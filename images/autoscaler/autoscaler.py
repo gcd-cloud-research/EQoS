48735443cbe7e9c81906c3743876694bf1c2b493
Pere Piñol <perepinol@gmail.com>
2020-05-02 04:40:58 -0700

    Improved implementation for autoscaler, missing scaling and descaling calls

--- images/autoscaler/autoscaler.py
+++ images/autoscaler/autoscaler.py
@@ -1,5 +1,7 @@
+import os
+import time
 from datetime import datetime, timedelta
-from signal import alarm, pause
+from signal import alarm, pause, SIGALRM, signal, SIG_IGN
 import requests
 import logging
 
@@ -7,7 +9,11 @@ from kubernetes import client, config as kubeconfig
 import json
 import sys
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
+
+def fromisoformat(usage_time):
+    return datetime.fromisoformat(usage_time[:usage_time.find('.') + 4])
 
 
 def from_json(class_to_instantiate, json_obj):
@@ -30,13 +36,15 @@ class Config:
                 max_load=0.8,
                 max_load_nowait=0.9,
                 wait_seconds=60,
-                tolerance=5
+                tolerance=5,
+                grace=30
         ):
             self.min_load = min_load
             self.max_load = max_load
             self.max_load_nowait = max_load_nowait
             self.wait_seconds = wait_seconds
             self.tolerance = tolerance
+            self.grace_period = grace
 
     def __init__(
             self,
@@ -59,71 +67,69 @@ class Config:
 
 class LoadTracker:
     def __init__(self, config):
-        self.overloaded = {}  # Key: container name. Value: number of times the container has not been overloaded
-        self.underloaded = {}  # Key: container name. Value: number of times the container has not been underloaded
+        self.overloaded = {}
+        self.underloaded = {}
+        self.grace = {}
         self.config = config.scaling
         self.threshold = config.scaling.wait_seconds / config.update_seconds
 
-    def process_performances(self, performances):
-        scale = []
-        descale = []
-        # Update overloaded and underloaded objects
-        for container, usage_list in performances.items():
-            for usage in usage_list:
-                scale += [container] if self._overload(container, usage) else []
-                descale += [container] if self._underload(container, usage) else []
+    def process_usage(self, container, usage):
+        if container in self.grace:
+            # If grace period is exceeded by measurements, remove container from grace
+            if self.grace[container] < fromisoformat(usage['time']):
+                del self.grace[container]
+            # Otherwise, do not process usage
+            else:
+                return 0
 
-        return scale, descale
-
-    def _overload(self, container, usage):
-        # If container is over threshold, add to overloaded
-        if max(usage['cpu'], usage['memory']) > self.config.max_load:
-            self.overloaded, ret_val = self._increment_count(
-                container,
-                self.overloaded,
-                1 if max(usage['cpu'], usage['memory']) < self.config.max_load_nowait else self.threshold
-            )
-            return ret_val
-
-        # Otherwise, if it is in overloaded, increment count and remove if tolerance is reached
-        elif container in self.overloaded.keys():
-            self.overloaded[container][1] += 1
-            if self.overloaded[container][1] >= self.config.tolerance:
+        # If container is over immediate scale threshold, scale and add to grace
+        if max(usage['cpu'], usage['memory']) > self.config.max_load_nowait:
+            if container in self.overloaded:
                 del self.overloaded[container]
+                self.grace[container] = fromisoformat(usage['time']) + \
+                    timedelta(seconds=self.config.grace_period)
+            return 1
 
-        return False
+        # Check if scaling or descaling is needed (only one can be True)
+        scale, self.overloaded = self._load_check(self.overloaded, lambda u: u > self.config.max_load, container, usage)
+        descale, self.underloaded = self._load_check(self.underloaded, lambda u: u < self.config.min_load, container, usage)
 
-    def _underload(self, container, usage):
-        # If container is under threshold, add to underloaded
-        if max(usage['cpu'], usage['memory']) < self.config.min_load:
-            self.underloaded, ret_val = self._increment_count(container, self.underloaded)
-            return ret_val
+        if scale:
+            return 1  # Scale up
+        if descale:
+            return -1  # Scale down
+        return 0  # Do nothing
 
-        # Otherwise, if it is in underloaded, increment count and remove if tolerance is reached
-        elif container in self.underloaded.keys():
-            self.underloaded[container][1] += 1
-            if self.underloaded[container][1] >= self.config.tolerance:
-                del self.underloaded[container]
+    def _load_check(self, load_object, criteria, container, usage):
+        # If usage means exceptional load...
+        if criteria(max(usage['cpu'], usage['memory'])):
+            # If not in object, add
+            if container not in load_object:
+                load_object[container] = LoadTracker._new_load_object(usage['time'])
 
-        return False
+            # If it is in object and time > wait_seconds, scale and add to grace
+            elif fromisoformat(usage['time']) > \
+                    load_object[container]['start_time'] + timedelta(seconds=self.config.wait_seconds):
+                del load_object[container]
+                self.grace[container] = fromisoformat(usage['time']) + \
+                    timedelta(seconds=self.config.grace_period)
+                return True, load_object
 
-    def _increment_count(self, container, target_dict, inc_val=1):
-        if container not in target_dict.keys():
-            target_dict[container] = [0, 0]  # [<exceptional load count>, <normal load count>]
-        target_dict[container][0] += inc_val
-        if target_dict[container][0] >= self.threshold:
-            del target_dict[container]
-            return target_dict, True
-        return target_dict, False
+        # If usage means no exceptional load, but container is in object, increment normal load counter
+        elif container in load_object:
+            load_object[container]['normal_load_count'] += 1
+            # If tolerance is reached, remove from overloaded
+            if load_object[container]['normal_load_count'] >= self.config.tolerance:
+                del load_object[container]
 
+        return False, load_object  # Catch-all for non-scaling cases
 
-def to_container_dict(json_data):
-    result = {}
-    for entry in json_data:
-        if entry['container'] not in result:
-            result[entry['container']] = []
-        result[entry['container']].append(entry['usage'])
-    return result
+    @staticmethod
+    def _new_load_object(time):
+        return {
+            'start_time': fromisoformat(time),
+            'normal_load_count': 0
+        }
 
 
 def monitor_containers(config):
@@ -135,18 +141,29 @@ def monitor_containers(config):
         res = requests.get(
             'http://mongoapi:8000/query/performance',
             data=json.dumps({
-                'usage.time': {'$gte': datetime.utcnow() - stepback_time},
-                '$sort': [('container', 1)]
+                'usage.time': {'$gte': (datetime.utcnow() - stepback_time).isoformat()},
+                'container': {'$exists': True},
+                '$sort': [('usage.time', 1)]
             }),
             timeout=config.update_seconds / 2
         )
+
+        logging.debug("Received %d documents" % (len(res.json())))
 
         if res.status_code != 200:
             logging.warning("Received code %d from mongoapi: %s" % (res.status_code, res.text))
             stepback_time += timedelta(seconds=config.update_seconds)  # In next request, ask for all missing data
 
         else:
-            scale, descale = load_tracker.process_performances(to_container_dict(res.json()))
+            for measurement in res.json():
+                scale_val = load_tracker.process_usage(measurement['container'], measurement['usage'])
+                if scale_val != 0:
+                    logging.info("Container %s in pod %s should be %sscaled" % (
+                        measurement['container'],
+                        measurement['pod'],
+                        'de' if scale_val < 0 else ''
+                    ))
+                logging.info("No actions needed for %s in %s" % (measurement['container'], measurement['pod']))
         pause()
 
 
@@ -156,9 +173,13 @@ if __name__ == '__main__':
     else:
         filename = 'config.json'
 
-    print(Config.load(filename))
+    def sign_debug(sig, stackframe):
+        print(logging.debug("(ALRM: %d) Received signal %d" % (SIGALRM, sig)))
 
-    with open("testperformance.json") as fh:
-        x = json.load(fh)
-    l = LoadTracker(Config.load(filename))
-    print(l.process_performances(to_container_dict(x)))
+    if os.fork() == 0:
+        signal(SIGALRM, sign_debug)
+        monitor_containers(Config.load(filename))
+    else:
+        os.wait()
+        print("!!!!", flush=True)
+        time.sleep(1000)
