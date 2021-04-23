ec585b3e2318fff7b98a64161ca4bc797767f7da
lluismas <16152037+LluisMas@users.noreply.github.com>
2021-04-23 13:07:59 +0200

    Add memory waster

---
+++ images/memorywaster/waster.py
@@ -0,0 +1,58 @@
+import os
+import signal
+from time import sleep
+import logging
+
+import pymongo
+
+logging.basicConfig(level=logging.INFO)
+TARGET_LOAD = 90
+TOLERANCE = 5
+
+
+def waster():
+    byeMemory = []
+    while True:
+        global shouldFill
+        if shouldFill:
+            byeMemory.append([0]*1000)
+
+
+if __name__ == '__main__':
+    with open('hostname') as fh:
+        host = fh.readline().strip()
+
+    global shouldFill
+    logging.info(host)
+    client = pymongo.MongoClient('mongodb://admin:toor@internaldb:27017')
+    shouldFill = True
+    pids = []
+
+    while True:
+        last_perf = list(client.ehqos.performance
+                         .find({'host': host})
+                         .sort([('usage.time', pymongo.DESCENDING)])
+                         .limit(1)
+                         )[0]
+        memory = last_perf['usage']['memory']
+        if memory >= 100:
+            logging.info(memory)
+        if memory < TARGET_LOAD - TOLERANCE:
+            pid = os.fork()
+            if pid == 0:
+                waster()
+                exit(1)
+            pids.append(pid)
+            logging.info('Scaled %d' % len(pids))
+
+        elif memory > TARGET_LOAD + TOLERANCE:
+            if not pids:
+                logging.warning('Should remove a child, but none are available')
+            else:
+                victim = pids.pop()
+                os.kill(victim, signal.SIGTERM)
+                os.waitpid(victim, 0)
+                logging.info('Descaled %d' % len(pids))
+        else:
+            logging.info("Memory is in ideal load")
+        sleep(5)
