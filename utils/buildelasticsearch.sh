3bb55d90a0e6f9156ffef02a0773a9cf79bd01c4
lluismas <16152037+LluisMas@users.noreply.github.com>
2021-06-09 17:07:09 +0200

    Automation: Add Monitor node

--- utils/buildelasticsearch.sh
+++ utils/buildelasticsearch.sh
@@ -1,3 +1,3 @@
 docker stop elasticsearch
 docker rm elasticsearch
-docker run -d --name elasticsearch --network=host -dp 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.12.1
+docker run -d --name elasticsearch --network=elastic -dp 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:7.12.1
