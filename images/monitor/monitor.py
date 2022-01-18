28145ddb7c72d97246ea65beb805e4a146c02d65
lluismas <16152037+LluisMas@users.noreply.github.com>
2022-01-18 12:59:43 +0100

    Monitor: Don't use full path

--- images/monitor/monitor.py
+++ images/monitor/monitor.py
@@ -4,5 +4,5 @@ import time
 if __name__ == "__main__":
     while True:
         if not os.fork():
-            os.execl("/usr/bin/python3.7", "/usr/bin/python3.7", "stats.py")
+            os.execlp("python3.7", "python3.7", "stats.py")
         time.sleep(1)
